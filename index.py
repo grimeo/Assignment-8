2228be44fd53a34b020b31417e593c5041b9adbd
Romeo <kilazlove@gmail.com>
2021-12-27 04:57:50 +0800

    begin the program 1, declare num1, num2 and num3 to accepts input

---
+++ index.py
@@ -0,0 +1,23 @@
+# Assignment 8
+
+# Program 1: Lottery
+# Create a program that ask 3 numbers (0-9) from the user.
+# Generate 3 random winning numbers (0-9)
+# Display “Winner” if all 3 input numbers matched the generated numbers Display ”You loss” if not
+# Display ”Try again y/n” after each game
+# If the user enter “y” the user will play again
+# if “n” the program will exit.
+
+
+print("Welcome to program 1 of Assignment 8!\n")
+
+num1 = str(input("Type the 1st number: "))
+num2 = str(input("Type the 2nd number: "))
+num3 = str(input("Type the 3rd number: "))
+
+# Program 2: Guess the number
+# Generate 1 random number (0-100)
+
+# Ask the user to guess the number
+# Display “Greater than” if the inputed number is greater than the random number Display “Less than” 
+# if the inputed number is less than the random number Repeat asking the user until the random number has been guessed correctly.
